fbc80db942f258c1a12d67c39f5fc8a4de28df9a
cloudfogpine <123803089+cloudfogpine@users.noreply.github.com>
2023-07-22 09:43:57 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
-//加载结束判断(手动控制)测试模式
-// 加载动画文件目录
-const gifPath = '/Loading/';
+// 页面加载完成后触发
+/*window.addEventListener('load', () => {
 
-document.addEventListener('DOMContentLoaded', () => {
-
-  // 生成随机数
-  const randomNum = Math.floor(Math.random() * 1);
-
-  // 拼接随机文件名
-  const loaderImg = gifPath + 'loader-' + randomNum + '.gif';
-
-  // 设置img的src为随机文件
-  document.querySelector('.loader').src = loaderImg;
-
-  // 3秒后隐藏动画
-  setTimeout(() => {
-    document.querySelector('.loader-mask').style.display = 'none';
-  }, 3000);
-
-});
-//加载结束判断(自动检测)
-
-// 设置加载动画文件的目录
-/*const gifPath = '/Loading/';
-
-// 页面加载完毕后执行
-window.addEventListener('load', () => {
-
-  // 生成一个随机数
-  const randomNum = Math.floor(Math.random() * 1);
-
-  // 拼接随机gif文件名
-  let loaderImg = gifPath + 'loader-' + randomNum + '.gif';
-
-  // 设置img标签的src为这个随机gif文件 
-  document.querySelector('.loader').src = loaderImg;
-
-  // 加载完成后隐藏动画
+  // 隐藏加载动画
   document.querySelector('.loader').style.display = 'none';
 
+  // 显示页面内容
+  document.querySelector('.content').style.display = 'block'; 
 });*/
+//测试模式，下面这段代码正常情况下是注释了，下面这段代码仅供测试使用
+document.addEventListener('DOMContentLoaded', () => {
+
+  setTimeout(() => {
+    document.querySelector('.loader').style.display = 'none'; 
+    document.querySelector('.content').style.display = 'block';
+  }, 500);
+
+});
+// 导航栏效果实现
